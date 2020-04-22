25aabf8281ee12b05dcf142bc243511d11d8dc82
Nilay Majorwar <nilaymajorwar@gmail.com>
2020-04-23 03:29:27 +0530

    Add Ctrl+Enter to post message

--- web/src/screens/chat-screen.js
+++ web/src/screens/chat-screen.js
@@ -19,24 +19,38 @@ const ChatScreen = (props) => {
   const activeChat = useSelector(getActiveChat);
   const profile = useSelector(getProfile);
 
-  const onPostMessage = (e) => {
-    e.preventDefault();
-    postMessage(activeChat._id, input);
-    // Optimistic UI - add message without
-    // waiting for server confirmation
-    const newMessage = {
-      authorId: {
-        _id: profile._id,
-        name: profile.name,
-      },
-      date: Date().toString(),
-      content: input,
-      isOwn: true,
+  const onPostMessage = React.useCallback(
+    (e) => {
+      e && e.preventDefault();
+      // Do not post empty messages
+      if (!input || !input.trim()) return;
+
+      postMessage(activeChat._id, input);
+      // Optimistic UI - add message without
+      // waiting for server confirmation
+      const newMessage = {
+        authorId: {
+          _id: profile._id,
+          name: profile.name,
+        },
+        date: new Date().toISOString(),
+        content: input,
+        isOwn: true,
+      };
+      const newMessageList = [...messages];
+      newMessageList.push(newMessage);
+      setMessages(newMessageList);
+    },
+    [input, activeChat._id, profile._id, profile.name, messages]
+  );
+
+  React.useEffect(() => {
+    const handler = (e) => {
+      if (e.keyCode === 13 && e.ctrlKey) onPostMessage();
     };
-    const newMessageList = [...messages];
-    newMessageList.push(newMessage);
-    setMessages(newMessageList);
-  };
+    document.addEventListener('keyup', handler);
+    return () => document.removeEventListener('keyup', handler);
+  }, [onPostMessage]);
 
   // Fetch messages on chat change
   React.useEffect(() => {
@@ -60,11 +74,11 @@ const ChatScreen = (props) => {
         <div className="cz-messages__input">
           <EuiPanel paddingSize="l" style={{ background: '#ebfdff' }} hasShadow>
             <EuiForm component="form">
-              <EuiFormRow fullWidth>
+              <EuiFormRow fullWidth helpText="Ctrl + Enter to post">
                 <EuiTextArea
                   fullWidth
                   value={input}
-                  rows={2}
+                  rows={3}
                   placeholder="Say something"
                   aria-label="Enter your comment"
                   onChange={(e) => setInput(e.nativeEvent.target.value)}
@@ -76,6 +90,7 @@ const ChatScreen = (props) => {
                   fill
                   style={{ alignSelf: 'flex-end' }}
                   onClick={onPostMessage}
+                  disabled={!input || !input.trim()}
                 >
                   Post comment
                 </EuiButton>
