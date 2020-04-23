1c989449252ed6a081f52e022510f1a46a6606e0
Nilay Majorwar <nilaymajorwar@gmail.com>
2020-04-23 20:18:50 +0530

    Add chat info panel and tweak message panel

--- web/src/screens/chat-screen.js
+++ web/src/screens/chat-screen.js
@@ -8,6 +8,7 @@ import {
   EuiButton,
   EuiForm,
   EuiTextAlign,
+  EuiEmptyPrompt,
 } from '@elastic/eui';
 import { getChatMessages, postMessage } from '../utils/requests';
 import MessageList from '../components/message-list';
@@ -16,6 +17,7 @@ const ChatScreen = (props) => {
   const [input, setInput] = React.useState('');
   const [loading, setLoading] = React.useState(true);
   const [messages, setMessages] = React.useState(null);
+  const inputPanelRef = React.useRef(null);
   const activeChat = useSelector(getActiveChat);
   const profile = useSelector(getProfile);
 
@@ -28,6 +30,7 @@ const ChatScreen = (props) => {
       postMessage(activeChat._id, input);
       // Optimistic UI - add message without
       // waiting for server confirmation
+      setInput('');
       const newMessage = {
         authorId: {
           _id: profile._id,
@@ -37,13 +40,13 @@ const ChatScreen = (props) => {
         content: input,
         isOwn: true,
       };
-      const newMessageList = [...messages];
-      newMessageList.push(newMessage);
+      const newMessageList = [...messages, newMessage];
       setMessages(newMessageList);
     },
     [input, activeChat._id, profile._id, profile.name, messages]
   );
 
+  // Ctrl+Enter keyboard shortcut
   React.useEffect(() => {
     const handler = (e) => {
       if (e.keyCode === 13 && e.ctrlKey) onPostMessage();
@@ -51,6 +54,12 @@ const ChatScreen = (props) => {
     document.addEventListener('keyup', handler);
     return () => document.removeEventListener('keyup', handler);
   }, [onPostMessage]);
+
+  // Scroll to bottom on messages change
+  React.useEffect(() => {
+    console.log('Scroll called on', inputPanelRef.current);
+    inputPanelRef.current.scrollIntoView({ behaviour: 'smooth' });
+  }, [messages, loading]);
 
   // Fetch messages on chat change
   React.useEffect(() => {
@@ -67,38 +76,66 @@ const ChatScreen = (props) => {
     })();
   }, [activeChat._id, profile._id]);
 
-  return (
+  const messagePanel = (
     <div className="cz-messages__wrapper">
-      <div className="cz-messages__middle">
-        <MessageList messages={messages} loading={loading}></MessageList>
-        <div className="cz-messages__input">
-          <EuiPanel paddingSize="l" style={{ background: '#ebfdff' }} hasShadow>
-            <EuiForm component="form">
-              <EuiFormRow fullWidth helpText="Ctrl + Enter to post">
-                <EuiTextArea
-                  fullWidth
-                  value={input}
-                  rows={3}
-                  placeholder="Say something"
-                  aria-label="Enter your comment"
-                  onChange={(e) => setInput(e.nativeEvent.target.value)}
-                />
-              </EuiFormRow>
-              <EuiTextAlign textAlign="right" style={{ marginTop: 10 }}>
-                <EuiButton
-                  type="submit"
-                  fill
-                  style={{ alignSelf: 'flex-end' }}
-                  onClick={onPostMessage}
-                  disabled={!input || !input.trim()}
-                >
-                  Post comment
-                </EuiButton>
-              </EuiTextAlign>
-            </EuiForm>
-          </EuiPanel>
-        </div>
+      <MessageList messages={messages} loading={loading}></MessageList>
+      <div className="cz-messages__input" ref={inputPanelRef}>
+        <EuiPanel paddingSize="l" style={{ background: '#ebfdff' }} hasShadow>
+          <EuiForm component="form">
+            <EuiFormRow fullWidth helpText="Ctrl + Enter to post">
+              <EuiTextArea
+                fullWidth
+                value={input}
+                rows={3}
+                placeholder="Say something"
+                aria-label="Enter your comment"
+                onChange={(e) => setInput(e.nativeEvent.target.value)}
+              />
+            </EuiFormRow>
+            <EuiTextAlign textAlign="right" style={{ marginTop: 10 }}>
+              <EuiButton
+                type="submit"
+                iconType="editorComment"
+                fill
+                size="s"
+                style={{ alignSelf: 'flex-end' }}
+                onClick={onPostMessage}
+                disabled={!input || !input.trim()}
+              >
+                Post comment
+              </EuiButton>
+            </EuiTextAlign>
+          </EuiForm>
+        </EuiPanel>
       </div>
+    </div>
+  );
+
+  const chatInfoPanel = (
+    <EuiPanel style={{ height: '100%' }} hasShadow paddingSize="l">
+      <EuiEmptyPrompt
+        iconType="editorComment"
+        title={<h2>{activeChat.title}</h2>}
+        body={
+          <p>
+            Navigators use massive amounts of spice to gain a limited form of
+            prescience. This allows them to safely navigate interstellar space,
+            enabling trade and travel throughout the galaxy.
+          </p>
+        }
+        actions={
+          <EuiButton color="primary" fill>
+            Harvest spice
+          </EuiButton>
+        }
+      />
+    </EuiPanel>
+  );
+
+  return (
+    <div className="cz-chat__wrapper">
+      <div className="cz-chat__messagepanel">{messagePanel}</div>
+      <div className="cz-chat__chatpanel">{chatInfoPanel}</div>
     </div>
   );
 };
