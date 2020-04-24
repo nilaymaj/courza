6d8e2dc47d63ce6a587ed7ed1de8e7458438aa37
Nilay Majorwar <nilaymajorwar@gmail.com>
2020-04-24 21:23:39 +0530

    Add scroll functions to chat screen

--- web/src/components/message-list/index.js
+++ web/src/components/message-list/index.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import { Element, animateScroll as scroll } from 'react-scroll';
 import { EuiLoadingContent, EuiSpacer, EuiEmptyPrompt } from '@elastic/eui';
 import Message from '../message';
 
@@ -17,9 +18,31 @@ type Props = {|
   messages: ?Array<MessageProps>,
 |};
 
+const scrollToBottom = (instant?: boolean) => {
+  scroll.scrollToBottom({
+    duration: instant ? 0 : 800,
+    smooth: !instant,
+    containerId: 'cz-messages__scroll',
+  });
+};
+
+// Uncomment when this function comes to use
+// const scrollToMessage = (messageId: string, instant?: boolean) => {
+//   scroller.scrollTo(messageId, {
+//     duration: instant ? 0 : 800,
+//     smooth: !instant,
+//     containerId: 'cz-messages__scroll',
+//   });
+// };
+
 const MessageList = (props: Props) => {
+  // Scroll to bottom when opened
+  React.useEffect(() => {
+    scrollToBottom(true);
+  }, [props.loading]);
+
   return (
-    <div className="cz-messages__list">
+    <div id="cz-messages__scroll" className="cz-messages__list">
       {props.loading ? (
         <div className="cz-messages__loading">
           <EuiLoadingContent></EuiLoadingContent>
@@ -31,13 +54,15 @@ const MessageList = (props: Props) => {
         </div>
       ) : props.messages && props.messages.length ? (
         props.messages.map((m) => (
-          <Message
-            name={m.authorId.name}
-            key={m._id || m.tempId}
-            content={m.content}
-            isOwn={m.isOwn}
-            date={m.date}
-          ></Message>
+          <Element name={m._id || m.tempId} key={m._id || m.tempId}>
+            <Message
+              name={m.authorId.name}
+              // key={m._id || m.tempId}
+              content={m.content}
+              isOwn={m.isOwn}
+              date={m.date}
+            ></Message>
+          </Element>
         ))
       ) : (
         <EuiEmptyPrompt
