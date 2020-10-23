448f7ede2590a6f61ae57e0699095a295b53f884
Nilay Majorwar <nilaymajorwar@gmail.com>
2020-10-23 19:29:39 +0530

    Remove route for full profile fetch

--- server/routes/student.ts
+++ server/routes/student.ts
@@ -1,11 +1,9 @@
 import { Request, Router } from 'express';
 import { IStudent } from '../models/student';
 import controller from './controller';
-import * as EnrolmentService from '../services/enrolmentService';
-import * as ThreadService from '../services/threadService';
 const router = Router();
 
-// Get basic student profile
+// Get student's profile
 interface IProfileReq extends Request {
   user: IStudent;
   body: {};
@@ -15,29 +13,6 @@ router.get(
   controller(async (req: IProfileReq, res) => {
     const { user } = req;
     return res.send(user.getInfo());
-  })
-);
-
-// Get full student profile
-// TODO: Remove this!
-interface IFullProfileReq extends Request {
-  user: IStudent;
-  body: {};
-}
-router.get(
-  '/fullprofile',
-  controller(async (req: IFullProfileReq, res) => {
-    const student = req.user;
-    const basicInfo = student.getInfo();
-    const courses = await EnrolmentService.getStudentCourses(student, true);
-    const fullCourses = await Promise.all(
-      courses.map(async (c) => {
-        // @ts-ignore
-        const threads = await ThreadService.getAll(c, true);
-        return { ...c, threads };
-      })
-    );
-    return res.send({ ...basicInfo, courses: fullCourses });
   })
 );
 
